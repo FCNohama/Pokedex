fdb1eb378a99f01dc52b1a4252cc8ada51b6b08a
Felipe <nohamaoficial@gmail.com>
2024-08-20 20:45:20 -0300

    feat: adjustments to functionality and variable naming

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-const pokemonName = document.querySelector('.pokemon__name');
-const pokemonNumber = document.querySelector('.pokemon__number');
-const pokemonImage = document.querySelector('.pokemon__image');
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_img');
 
 const form = document.querySelector('.form');
-const input = document.querySelector('.input__search');
+const input = document.querySelector('.input_search');
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
